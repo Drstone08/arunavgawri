a3fc0e346ba105c77dd48a5cf2c944701397bace
DoctorStone08 <arunavgawri@gmail.com>
2024-12-30 22:56:32 +0530

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss";
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),ghPages()],
+  base:'arunavgawri/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
